83eefe52343f88bb47061059451493e335243f59
Necro Programmer <Morbid6dead@gmail.com>
2021-10-19 12:53:33 +0300

    почти истина

--- main.py
+++ main.py
@@ -1,34 +1,32 @@
 from os import system
 from re import compile, escape
-   
-#название программы, которую мы ищем
-search_bdo = compile(escape(' '))
 
-def recursion(command):
-    system('tasklist /NH /SVC > processes.txt')
-    with open('processes.txt', 'r') as logs:
+def cmd_processes(cmd):
+    system(cmd)
+
+def exam_process(file, prcs):
+    with open(file) as logs:
         lines = logs.readlines()
-        for line in lines:
-            bdo = command.search(line)
-            if bdo is None:
-                continue
-            else:
-                with open('bdo.txt', 'w') as bdo:
-                    bdo.write('done')
-                    bdo.close()
-                    logs.close()
-                with open('bdo.txt', 'r') as bdo:
-                    exam = bdo.readlines()
-                    for line in exam:
-                        if line is None:
-                            bdo.close()
-                            break
-                        else:
-                            try:
-                                recursion(search_bdo)
-                            except RecursionError:
-                                continue
+    for line in lines:
+        proc = prcs.search(line)
+        if proc is None:
+            continue
+        else:
+            logs.close()
+            var=0
+        while var == 1:
+            system('del {file}')
+            break
+        else:
+            print('done')
 
-recursion(search_bdo)
-system('del bdo.txt')
-system('del processes.txt')
+filename = 'processes.txt'
+command = f'tasklist /NH /SVC > {filename}'
+
+process = compile(escape('putty.exe'))
+
+while True:
+    cmd_processes(command)
+    exam_process(filename, process)
+
+
