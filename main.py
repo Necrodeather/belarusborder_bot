ada43c08be6e88ef0f62ce10184281584048cee8
Necro Programmer <Morbid6dead@gmail.com>
2021-12-06 22:25:16 +0300

    add exception

--- main.py
+++ main.py
@@ -7,30 +7,57 @@ from os import system
 #Текст
 category = 'Категория Транспортного средства\n1.Легковой автомобиль\n2.Грузовой автомобиль\n3.Автобус\n4.Легковой/пассажирский микроавтобус\n5.Мотоцикл'
 сheckpoint  = 'Пункт пропуска\n1.Брест\n2.Урбаны\n3.Бенякони\n4.Котловка\n5.Григоровщина'
+timing = '\n00-01 01-02 02-03 03-04 04-05 05-06\n06-07 07-08 08-09 09-10 10-11 11-12\n12-13 13-14 14-15 15-16 16-17 17-18\n18-19 19-20 20-21 21-22 22-23 23-00'
+InputError = 'Ошибка ввода данных!'
 #Отключение логов драйвера
 options = webdriver.ChromeOptions()
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
 #Драйвер
 driver = webdriver.Chrome(options=options)
-#########################Редактировать под условия!################################
-print(category)
-input_checkbox_one = int(input('Укажите цифрой данную категорию: '))
-print(сheckpoint)
-input_checkbox_two = int(input('Укахите цифрой данный пункт пропуска: '))
 
-input_day = int(input("Введите день: "))
-input_mouth = int(input("Введите месяц: "))
-input_year = int(input("Введите год: "))
+#########################Исключения############################################
+def input_errors(ico,ict,id,im,iy):
+        if len(str(ico)) and len(str(ict)) == 1:
+                pass
+        elif len(str(id)) and len(str(im)) == 2 and len(str(iy)) == 4:
+                pass
+        else:   #WTF????!!!
+                driver.close
+                system('cls')
+                print(InputError)
+                main()
+########################Ввод данных#############################################
+def input_dates():
+        try:
+                print(category)
+                input_checkbox_one = int(input('Укажите цифрой данную категорию: '))
+                print(сheckpoint)
+                input_checkbox_two = int(input('Укахите цифрой данный пункт пропуска: '))
 
-input_date = str(input_day) + '.' + str(input_mouth) + '.' + str(input_year)
-timing = '\n00-01 01-02 02-03 03-04 04-05 05-06\n06-07 07-08 08-09 09-10 10-11 11-12\n12-13 13-14 14-15 15-16 16-17 17-18\n18-19 19-20 20-21 21-22 22-23 23-00'
-print('Выберите время бронирования из перечисленного: '+timing)
-input_time = input("Время: ")
+                input_day = int(input("Введите день: "))
+                input_mouth = int(input("Введите месяц: "))
+                input_year = int(input("Введите год: "))
+                input_errors(input_checkbox_one, input_checkbox_two, input_day, input_mouth, input_year)
+                input_date = str(input_day) + '.' + str(input_mouth) + '.' + str(input_year)
+                print('Выберите время бронирования из перечисленного: '+timing)
+                input_time = input("Время: ")
+
+                return input_checkbox_one, input_checkbox_two, input_date, input_time
+        except ValueError:
+                system('cls')
+                print("Неправильное значение, введите заново")
+                input_dates()
+
 #################################################################################
 #Кнопки
 def checkbox_click(i):
-        checkbox = driver.find_elements_by_class_name("label")
-        checkbox[i-1].click()
+        try:
+                checkbox = driver.find_elements_by_class_name("label")
+                checkbox[i-1].click()
+        except IndexError:
+                system('cls')
+                print(InputError)
+                input_dates()
 
 def btn_next_id_click():
         btn_next_id = driver.find_element_by_id("next")
@@ -120,8 +147,9 @@ class bot_step(object):
 ###################Осталось написать автоматическую авторизацию##########################################
 
 def main():
-        first_step = belarusborder_bot(driver,input_checkbox_one,input_checkbox_two)
-        second_step = bot_step(driver,input_date, input_time)
+        input_checkbox_one, input_checkbox_two, input_date, input_time = input_dates()
+        first_step = belarusborder_bot(driver, input_checkbox_one, input_checkbox_two)
+        second_step = bot_step(driver, input_date, input_time)
         first_step.first_queue()
         second_step.second_queue()
         rebooting()
@@ -137,6 +165,7 @@ def rebooting():
                         system('cls')
                         print('Неверное значение\n')
                         rebooting()        
+
 
 if __name__ == '__main__':
         main()
