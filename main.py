9550a0591361fa000896da3a079cfa27e13b6238
Necro Programmer <Morbid6dead@gmail.com>
2022-03-03 07:50:27 +0300

    end

--- main.py
+++ main.py
@@ -2,23 +2,59 @@ from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from email.mime.text import MIMEText
 from time import sleep
 from winsound import Beep
 from os import system
-import json, random
+import json, smtplib, random
+
+
+with open("autorization.json", "r") as json_email:
+    auto = json.load(json_email)
+
+
+def send_email(message):
+
+    sender = auto["Email_login"]
+    password = auto["Email_password"]
+    send = auto["to_Send"]
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.ehlo()
+    server.starttls()
+
+    try:
+        server.login(sender, password)
+        msg = MIMEText(message, 'plain', 'utf-8')
+        msg['Subject'] = '[WARNING] Найдена бронь!'
+
+        server.sendmail(sender, send, msg.as_string())
+        return print("Оповещение успешно отправлено!")
+    except Exception as _ex:
+        return print(f"{_ex}\nПроверьте правильность написания логина и пароля!")
+
 
 # Текст
+category = 'Категория Транспортного средства\n1.Легковой автомобиль\n2.Грузовой автомобиль\n3.Автобус\n4.Легковой/пассажирский микроавтобус\n5.Мотоцикл'
 сheckpoint = 'Пункт пропуска\n1.Урбаны\n2.Бенякони\n3.Каменный Лог\n4.Котловка\n5.Григоровщина'
+text_choice = 'Выберите стиль работы:\n1.Автоматический\n2.Определенное время'
 timing = '\n00-01 01-02 02-03 03-04 04-05 05-06\n06-07 07-08 08-09 09-10 10-11 11-12\n12-13 13-14 14-15 15-16 16-17 17-18\n18-19 19-20 20-21 21-22 22-23 23-00'
 InputError = 'Ошибка ввода данных при выборе категории ТС и пункта пропуска!'
 # Массив
 tonws = ['Урбаны', 'Бенякони', 'Каменный Лог', 'Котловка', 'Григоровщина']
 # Драйвер
-driver = webdriver.Firefox()
+options = webdriver.ChromeOptions()
+options.add_argument("--disable-blink-features=AutomationControlled")
+options.add_argument(rf'user-data-dir=C:\Users\{auto["User"]}\AppData\Local\Google\Chrome\User Data')
+options.add_argument(f'--profile-directory={auto["Profile"]}')
+options.add_experimental_option('excludeSwitches', ['enable-logging'])
+driver = webdriver.Chrome(options=options)
+
 ########################Ввод данных#############################################
+
 def input_category():
     try:
-        input_checkbox_one = 1
+        print(category)
+        input_checkbox_one = int(input('Укажите цифрой данную категорию: '))
         print(сheckpoint)
         input_checkbox_two = int(
             input('Укахите цифрой данный пункт пропуска: '))
@@ -51,10 +87,16 @@ def input_dates():
 
 
 def input_choice():
-    input_choice = 2
+    print(text_choice)
+    try:
+        input_choice = int(input('Укажите цифрой определенный пункт: '))
+    except ValueError:
+        print("Неправильное значение, введите заново")
+        input_times()
 
     if input_choice == 1:
-        pass
+        input_time = None
+        return input_time
     elif input_choice == 2:
         input_time = input_times()
         return input_time
@@ -71,10 +113,11 @@ def reboot():
         quit()
 ######################Кнопки################################################
 
-
 def checkbox_click(i):
     checkbox = driver.find_elements(By.CLASS_NAME, ("label"))
     checkbox[i-1].click()
+    sleep(3)
+
 
 
 def btn_next_id_click():
@@ -96,6 +139,7 @@ def slice(test):
 def nav_btn():
     nav_btn_click = driver.find_element(By.CLASS_NAME, ("nav-btn"))
     nav_btn_click.click()
+    sleep(3)
 
 ###########################Авторизация##################################
 
@@ -157,8 +201,6 @@ class belarusborder_bot(object):
         btn_next_class_click()
 
 # Прохождение основного этапа для ловли брони.
-
-
 class bot_step(object):
 
     def __init__(self, webdriver, date, town, time=None):
@@ -183,8 +225,37 @@ class bot_step(object):
             i = random.uniform(0.3, 1.5)
             sleep(i)
             self.webdriver.refresh()
-            self.cycle_two_time()
+            self.cycle_fisrt_time()
         
+
+    def cycle_fisrt_time(self):
+
+        self.check_time = self.webdriver.find_elements(By.CLASS_NAME, (
+            "intervalAvailable"))
+        if self.time == None:
+            self.first_choice()
+        else:
+            for time in self.check_time:
+
+                if slice(time) == self.time:
+                    time.click()
+                else:
+                    continue
+            self.cycle_two_time()
+
+    def first_choice(self):
+        self.output_url = self.webdriver.current_url
+        self.output_date = self.output_url[-10:].replace("=", "")
+        try:
+            self.check_time[0].click()
+            self.output_check_time = self.check_time[0].text
+            
+            self.checking_captcha()
+            
+            sleep(3600)
+            
+        except IndexError:
+            return False
 ############################КАПЧА##################################
     def checking_captcha(self):
         sleep(0.5)
@@ -217,6 +288,8 @@ class bot_step(object):
                 sleep(0.25)
                 system('cls')
                 print('Найдена бронь!')
+        send_email(
+                f'Найдена бронь на аккаунте: {auto["Login"]}\nДата: {self.output_date}\nПункт: {self.town}\nВремя: {self.output_check_time[:5]}\nURL: {self.output_url}')
         self.webdriver.implicitly_wait(5)
         self.info = self.webdriver.find_elements(By.CLASS_NAME, ('form-control'))
         self.info[-1].send_keys(f'{self.town} {self.output_check_time[:5]} {self.output_date[:5]}')
@@ -235,22 +308,21 @@ class bot_step(object):
                 
                 self.checking_captcha()
 
+                sleep(3600)
+
         except NoSuchElementException:
             return False
 
 
 def main():
-    try:
-        input_checkbox_one, input_checkbox_two = input_category()
-        town = tonws[input_checkbox_two - 1]
-        input_date = input_dates()
-        first_step = belarusborder_bot(
-            driver, input_checkbox_one, input_checkbox_two)
-        second_step = bot_step(driver, input_date, town, input_choice())
-        first_step.first_queue()
-        second_step.second_queue()
-    except Exception as _:
-        print(_)
+    input_checkbox_one, input_checkbox_two = input_category()
+    town = tonws[input_checkbox_two - 1]
+    input_date = input_dates()
+    first_step = belarusborder_bot(
+        driver, input_checkbox_one, input_checkbox_two)
+    second_step = bot_step(driver, input_date, town, input_choice())
+    first_step.first_queue()
+    second_step.second_queue()
 
 
 if __name__ == '__main__':
